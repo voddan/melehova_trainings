7c2e519e6551312885369f35778785937481a0cf
voddan <dgvodopyan@gmail.com>
2016-03-10 13:05:28 +0300

    test-30

---
+++ task_3-measure-getc/test_30.c
@@ -0,0 +1,21 @@
+//
+// Created by voddan on 10/03/16.
+//
+
+#include <stdio.h>
+
+#define ROUNDS (30)
+
+int main(int argn, char ** args) {
+    printf("start\n");
+
+    FILE * input = fopen("/dev/random", "ro");
+
+    for (int i = 0; i < ROUNDS; ++i) {
+        getc(input);
+    }
+
+    printf("stop\n");
+
+    return 0;
+}
